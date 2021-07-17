073ed429be8b25e51c0e349af1167f1fc6f8184e
ymd20 <ymd20@DESKTOP-FA5I8Q6>
2021-07-18 02:46:28 +0900

    データベース関連の追加

---
+++ src/models/Task.java
@@ -0,0 +1,62 @@
+package models;
+
+import java.sql.Timestamp;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="tasks")
+
+public class Task {
+    @Id
+    @Column(name="id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name="toDo", length=255, nullable=false)
+    private String toDo;
+
+    @Column(name="detail", length=255, nullable=false)
+    private String detail;
+
+    @Column(name="deadline", nullable=false)
+    private Timestamp deadline;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getToDo() {
+        return toDo;
+    }
+
+    public void setToDo(String toDo) {
+        this.toDo = toDo;
+    }
+
+    public String getDetail() {
+        return detail;
+    }
+
+    public void setDetail(String detail) {
+        this.detail = detail;
+    }
+
+    public Timestamp getDeadline() {
+        return deadline;
+    }
+
+    public void setDeadline(Timestamp deadline) {
+        this.deadline = deadline;
+    }
+
+}
